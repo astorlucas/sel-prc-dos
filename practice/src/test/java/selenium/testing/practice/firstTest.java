eeda6d48e154434923e6b5153459d6e478a0b9f0
astorlucas <lucasastor@gmail.com>
2021-07-20 00:16:04 -0300

    webdrivermanager y running from cmd

--- practice/src/test/java/selenium/testing/practice/firstTest.java
+++ practice/src/test/java/selenium/testing/practice/firstTest.java
@@ -1,14 +1,32 @@
 package selenium.testing.practice;
-
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class firstTest {
-	public void elTest() {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+import io.github.bonigarcia.wdm.WebDriverManager;
 
+public class FirstTest {
+	@BeforeClass
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@Test
+	public void realTest() {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.hola.com/");
+		driver.get("https://www.saucedemo.com/");
 		driver.quit();
 	}
+
+	public void elTest() {
+		// Forma estandar de crear el chrome driver pero sin control de versionado
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * "src/main/resources/chromedriver.exe"); WebDriver driver = new
+		 * ChromeDriver(); driver.get("https://www.hola.com/"); driver.quit();
+		 */
+		// Forma con control de versionado importando el WebDriverManager
+	}
+
 }
